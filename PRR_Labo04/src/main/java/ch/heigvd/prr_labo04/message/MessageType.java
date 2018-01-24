f9d36ba5c444296e3a301da5ec6c6a7dcbe4b330
Viking & Robot <vikingandrobot@gmail.com>
2018-01-24 18:41:22 +0100

    Added MessageType class.

---
+++ PRR_Labo04/src/main/java/ch/heigvd/prr_labo04/message/MessageType.java
@@ -0,0 +1,16 @@
+/**
+ * File: MessageType.java
+ * Date: 24.01.2018
+ * Authors: Sathiya Kiruhsnapillai & Mathieu Monteverde
+ */
+
+package ch.heigvd.prr_labo04.message;
+
+/**
+ * Class that enumerate the message types.
+ */
+public class MessageType {
+   public static final byte REQUEST = 0;
+   public static final byte TOKEN = 1;
+   public static final byte END = 2;
+}
