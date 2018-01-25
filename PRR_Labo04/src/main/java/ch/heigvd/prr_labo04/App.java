c2f152431f1ee0c5887771fdb56b126be04b6c48
Viking & Robot <vikingandrobot@gmail.com>
2018-01-25 10:30:40 +0100

    Commented.

--- PRR_Labo04/src/main/java/ch/heigvd/prr_labo04/App.java
+++ PRR_Labo04/src/main/java/ch/heigvd/prr_labo04/App.java
@@ -1,3 +1,9 @@
+/**
+ * File: App.java
+ * Date: 24.01.2018
+ * Authors: Sathiya Kiruhsnapillai & Mathieu Monteverde
+ */
+
 package ch.heigvd.prr_labo04;
 
 import ch.heigvd.prr_labo04.configuration.Configuration;
@@ -9,11 +15,34 @@ import java.util.logging.Logger;
 import java.util.Scanner;
 
 /**
- *
+ * The App class is the entry point of the program for this project. The main 
+ * logic and the implementation of the termination algorithm is located in the 
+ * SiteManager class. 
+ * 
+ * The main method of the App class creates the resources to run the SiteManager
+ * (configuration and such) and offers a text interface in the command line
+ * to start tasks and/or to forbid the creation of new tasks. 
+ * 
+ * Each program must be given it's site Id and the number of sites 
+ * in the system when executed. 
+ * 
+ * java -jar PRR_Labo04-with-dependencies-0.1.jar <id> <numberOfSites>
+ * 
+ * For example, run : 
+ * 
+ * java -jar PRR_Labo04-with-dependencies-0.1.jar 0 4
+ * 
+ * We made the assumption that the site of id 0 starts as the holder of the token to
+ * pass around when the site become inactive. This means that when starting the whole
+ * systema, the site to start the first task (or request the termination) must be 
+ * the site of id 0. Otherwise, the site executing said commands won't be the holder
+ * of the token and won't be able to request the termination of the system 
+ * if the tasks directly end.
  */
 public class App {
    public static void main(String[] args) throws SocketException {
       
+      // The program must be executed with two arguments
       if (args.length != 2) {
          System.out.println("Le programme doit recevoir en paramètre son ID de site"
                  + " (0 à 3) et le nombre de sites.");
@@ -21,22 +50,27 @@ public class App {
          System.exit(1);
       }
       
+      // Get the Id of the current site
       int id = Integer.parseInt(args[0]);
       
+      // The Id must be inside [1, 3]
       if (id < 0 || id > 3) {
          System.out.println("Erreur: l'ID de site doit être dans l'intervalle doit être "
                  + "entre 0 et 3 compris.");
          System.exit(1);
       }
       
+      // Get the number of sites
       int n = Integer.parseInt(args[1]);
       
+      // The number of sites must be inside [1, 4]
       if (n < 1 || n > 4) {
          System.out.println("Erreur: le nombre de sites doit être dans l'intervalle doit être "
                  + "entre 1 et 4 compris.");
          System.exit(1);
       }
       
+      // Load the configuration
       Configuration config;
       try {
          Configuration.loadConfiguration("/processes.txt", id, n);
@@ -79,7 +113,7 @@ public class App {
                case 2:
                   System.out.println("End the application");
                   System.out.println("");
-                  manager.requestTerminate();
+                  manager.fordidNewTasks();
                   running = false;
                   break;
                default:
--- PRR_Labo04/src/main/java/ch/heigvd/prr_labo04/task/SiteManager.java
+++ PRR_Labo04/src/main/java/ch/heigvd/prr_labo04/task/SiteManager.java
@@ -21,45 +21,75 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Class that manages the site in the system.
+ * The SiteManager class is the main class of the Termination algorithm
+ * for this project. It allows to create new tasks on any of the sites of 
+ * the system and also provides a way to forbid the creation of new 
+ * tasks on the current site. 
+ * 
+ * For this project, we tried to implement the given ADA algorithm as close 
+ * as possible. We decided to have the SiteManager loop in a separate Thread and 
+ * do all the significant actions in said loop. 
+ * 
+ * The SiteManager keeps a queue of messages (Message class instances for convenient
+ * storage) and in each execution of the loop, the thread either reads and process
+ * a message from that queue, or wait if the queue is empty. 
+ * 
+ * Each call to action such as receiving a message from another site oe requests 
+ * to create a new task from the current site simply adds a Message representing
+ * the event in the queue and notifies the potentially waiting Thread that 
+ * new actions can be taken.
+ * 
+ * Any concurrent action to the main SiteManager thread follows the same logic: modifying 
+ * state variable and notify said thread that new actions can be taken (with a
+ * notify() call). 
+ * 
+ * The SiteManager uses internal state variable to decide what to do.
+ * 
+ * The SiteManager either receives a message of which it is the recipient, or
+ * it receives a message meant for his neighbour or for a site further in the 
+ * network ring. In the first case, it processes the message, or sent it further 
+ * in the second case.
+ * 
  */
 public class SiteManager implements TaskManager, MessageListener {
-   
+
    // The id of the current site
    private final int siteId;
-   
+
    // The configuration of the site system
    private final Configuration config;
 
    // List of received messages
    private final List<Message> messages;
-   
+
    // Number of tasks currently executing
    private int numberOfTasks;
-   
+
+   // MessageReceiver to receive messages
    private final MessageReceiver messageReceiver;
-   
+
    // Socket to send messages
    private final DatagramSocket sendSocket;
-   
+
    // Whether or not other site has sent the END message
    private boolean terminate;
-   
-   // True if a request to terminate from the same site has benn made
+
+   // True if the creation of new tasks is allowed
    private boolean allowNewTasks;
-   
+
    // Whether or not 
    private boolean hasToken;
-   
+
    // Wether or not the site has been active since the token was sent
    private boolean hasBeenActive;
 
    /**
     * Constructor
+    *
     * @throws SocketException if it can't open the sending socket
     */
    public SiteManager() throws SocketException {
-      
+
       // Initialize attributes
       numberOfTasks = 0;
       messages = new ArrayList<>();
@@ -68,11 +98,10 @@ public class SiteManager implements TaskManager, MessageListener {
       this.hasToken = this.siteId == 0;
       this.hasBeenActive = false;
       this.allowNewTasks = true;
-      
-      
+
       // Create the socket to send messages
       sendSocket = new DatagramSocket();
-      
+
       // Create the MessageReceiver
       messageReceiver = new MessageReceiver(
               Configuration.getConfiguration().getSite(this.siteId).getValue(), this
@@ -85,8 +114,8 @@ public class SiteManager implements TaskManager, MessageListener {
     */
    public void start() {
       Thread t = new Thread(() -> {
-         
-         // In a loop, manage received messages
+
+         // In a loop, manage received messages and take action
          while (true) {
             synchronized (SiteManager.this) {
                // Wait if there is no message in the list
@@ -98,7 +127,7 @@ public class SiteManager implements TaskManager, MessageListener {
                   }
                }
             }
-            
+
             // Check if the site must terminate
             if (getTerminate() && !isActive()) {
                // Terminate
@@ -107,22 +136,21 @@ public class SiteManager implements TaskManager, MessageListener {
                closeResources();
                return;
             }
-            
+
             // If we have messages to read
             if (!messages.isEmpty()) {
-               
-               // Get the oldest one
+
+               // Get the oldest one and remove it from the list
                Message message;
-               synchronized(SiteManager.this) 
-               {
+               synchronized (SiteManager.this) {
                   message = messages.get(0);
                   messages.remove(0);
                }
-               
+
                // Switch on message types
                switch (message.getType()) {
                   case MessageType.REQUEST:
-                     
+
                      // If we are the recipient
                      if (message.getRecipient() == this.siteId) {
                         // Start a new task
@@ -133,42 +161,42 @@ public class SiteManager implements TaskManager, MessageListener {
                      }
                      hasBeenActive = true;
                      break;
-                  
+
                   case MessageType.TOKEN:
                      hasToken = true;
                      if (!isActive() && !hasBeenActive) {
                         sendMessage(
                                 new Message(
-                                        (this.siteId + 1) % config.getNumberOfSites(), 
+                                        (this.siteId + 1) % config.getNumberOfSites(),
                                         MessageType.END
                                 )
                         );
                      } else if (!isActive()) {
                         sendMessage(
-                                 new Message(
-                                         (this.siteId + 1) % config.getNumberOfSites(), 
-                                         MessageType.TOKEN
-                                 )
-                         );
-                         hasBeenActive = false;
-                         hasToken = false;
+                                new Message(
+                                        (this.siteId + 1) % config.getNumberOfSites(),
+                                        MessageType.TOKEN
+                                )
+                        );
+                        hasBeenActive = false;
+                        hasToken = false;
                      }
                      break;
-                     
+
                   case MessageType.END:
                      // If the request of termination hasn't been made yet
                      if (!getTerminate()) {
                         setTerminate(true);
-                        
+
                         // Send the message to our neighbour
                         sendMessage(
                                 new Message(
-                                        (this.siteId + 1) % config.getNumberOfSites(), 
+                                        (this.siteId + 1) % config.getNumberOfSites(),
                                         MessageType.END
                                 )
                         );
                      }
-                     
+
                      // If there is no task excuting, end the SiteManager 
                      if (!isActive()) {
                         System.out.println("Ending application after receiving an END message.");
@@ -183,7 +211,7 @@ public class SiteManager implements TaskManager, MessageListener {
                   if (hasToken) {
                      sendMessage(
                              new Message(
-                                     (this.siteId + 1) % config.getNumberOfSites(), 
+                                     (this.siteId + 1) % config.getNumberOfSites(),
                                      MessageType.TOKEN
                              )
                      );
@@ -197,14 +225,14 @@ public class SiteManager implements TaskManager, MessageListener {
       });
       t.start();
    }
-   
+
    /**
     * Close the resources
     */
    private void closeResources() {
       messageReceiver.stop();
    }
-   
+
    @Override
    public synchronized void messageReceived(Message message) {
       // Add the message to the list and notify the SiteManager
@@ -226,55 +254,60 @@ public class SiteManager implements TaskManager, MessageListener {
    public void startNewTaskOnSite(int siteId) {
       // If asking for current site to do a task
       if (siteId == this.siteId) {
-         synchronized(this) {
+         synchronized (this) {
             messages.add(new Message(this.siteId, MessageType.REQUEST));
             this.notify();
          }
          return;
       }
-      
+
       // Send a message
       sendMessage(new Message(siteId, MessageType.REQUEST));
    }
-   
+
    /**
-    * Request to terminate the system
+    * Forbid the creation of new tasks on the current site past the moment this
+    * method has been called.
     */
-   public synchronized void requestTerminate() {
+   public synchronized void fordidNewTasks() {
       allowNewTasks = false;
+      this.notify();
    }
-   
+
    /**
     * Get wether or not a termination request has been made
+    *
     * @return true if a termination request has been made
     */
    private synchronized boolean getTerminate() {
       return terminate;
    }
-   
+
    /**
     * Set the termination request
-    * @param requestTerminate true to make a termination request, false otherwise
+    *
+    * @param requestTerminate true to make a termination request, false
+    * otherwise
     */
    private synchronized void setTerminate(boolean requestTerminate) {
       this.terminate = requestTerminate;
    }
-   
+
    /**
     * Send a message to a site by sending to the current site neighbour
+    *
     * @param message the message to send
     */
    private void sendMessage(Message message) {
-      
+
       // Get the neighbour data
       InetAddress ipAddress = config.getSite((this.siteId + 1) % config.getNumberOfSites()).getKey();
       int port = config.getSite((this.siteId + 1) % config.getNumberOfSites()).getValue();
-      
+
       // Construct the message
       byte[] buf = new byte[2];
       buf = Message.messageToByteBuffer(message, buf);
-      
-      
+
       try {
          // Send message
          sendSocket.send(new DatagramPacket(
@@ -296,12 +329,13 @@ public class SiteManager implements TaskManager, MessageListener {
          Task t = new Task(this);
          t.execute();
          ++numberOfTasks;
-         System.out.println("A task has started. Remaining: " + numberOfTasks );
+         System.out.println("A task has started. Remaining: " + numberOfTasks);
       }
    }
-   
+
    /**
     * Get if there are tasks currently executing
+    *
     * @return true if there are tasks currently executing
     */
    public boolean isActive() {
